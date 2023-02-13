71a44af1421bbe567d20110c1d791e556ef3d842
Graham Anderson <gramman87@gmail.com>
2023-02-13 15:44:11 -0700

    first commit

---
+++ src/test/java/com/fbscolorado/documentworkflow/DocumentWorkflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fbscolorado.documentworkflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentWorkflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
