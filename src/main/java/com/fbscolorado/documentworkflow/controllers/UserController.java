0c983a7e957e1447ffe4e7b80d1fef646f44e36b
Graham Anderson <gramman87@gmail.com>
2023-03-02 09:36:56 -0700

    prelim userController stubed out

--- src/main/java/com/fbscolorado/documentworkflow/controllers/UserController.java
+++ src/main/java/com/fbscolorado/documentworkflow/controllers/UserController.java
@@ -1,7 +1,9 @@
 package com.fbscolorado.documentworkflow.controllers;
 
 import com.fbscolorado.documentworkflow.entities.User;
+import com.fbscolorado.documentworkflow.entities.Workflow;
 import com.fbscolorado.documentworkflow.services.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -26,8 +28,7 @@ public class UserController {
     }
 
     @GetMapping("users/{id}")
-    public User showUser(Principal principal, @RequestBody User user, @PathVariable Integer id,
-                         HttpServletResponse res) {
+    public User showUser(Principal principal, @RequestBody User user, @PathVariable Integer id, HttpServletResponse res) {
         try {
             if (principal.getName().equals(user.getUsername())) {
                 return userService.updateUser(principal.getName(), id, user);
@@ -37,6 +38,61 @@ public class UserController {
             res.setStatus(400);
             user = null;
         }
-        return null;
+        return user;
     }
+
+    @PostMapping("users")
+    public User createUser(@RequestBody User user, HttpServletRequest req, HttpServletResponse res) {
+        try {
+            userService.createUser(user);
+            res.setStatus(201);
+            StringBuffer url = req.getRequestURL();
+            url.append("/").append(user.getId());
+            res.setHeader("Location", url.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println("INVALID ENTRY FOR NEW User");
+            user = null;
+        }
+        return user;
+    }
+
+    @PutMapping("users/{id}")
+    public User updateUser(Principal principal, @PathVariable Integer id, @RequestBody User user, HttpServletRequest req, HttpServletResponse res) {
+        try {
+            user = userService.updateUser(principal.getName(), id, user);
+            if (user == null) {
+                res.setStatus(404);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            res.setStatus(400);
+            user = null;
+        }
+        return user;
+    }
+
+    @DeleteMapping("users/{id}")
+    public void deleteUser(@PathVariable Integer id, HttpServletResponse res) {
+        try {
+            if (userService.deleteUser(id)) {
+                res.setStatus(204);
+            } else {
+                res.setStatus(404);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            res.setStatus(400);
+        }
+    }
+
+    @GetMapping("users/{id}/workflows")
+    public List<Workflow> userWorkflows(@PathVariable Integer id, HttpServletResponse res) {
+        List<Workflow> workflows = userService.findWorkflowsByUserId(id);
+        if (workflows == null) {
+            res.setStatus(404);
+        }
+        return workflows;
+    }
+    
 }
--- src/main/java/com/fbscolorado/documentworkflow/services/UserServiceImpl.java
+++ src/main/java/com/fbscolorado/documentworkflow/services/UserServiceImpl.java
@@ -39,7 +39,7 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public User updateUser(String email, int id, User user) {
+    public User updateUser(String username, int id, User user) {
         Optional<User> optionalUser = userRepo.findById(id);
         User managedUser;
         if (optionalUser.isPresent()) {
